98074b24c44ddf2060d9321c88c1c2fdbd5da2f1
condini-mastheus <matheus.condini@gmail.com>
2018-08-29 22:40:14 -0300

    Add eslint airbnb styles and adpting App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    )
-  }
-}
+const App = () => <div className="App" />
 
 export default App
