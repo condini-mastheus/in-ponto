070e1be56ddc488921c0d3189798555ddd864355
condini-mastheus <matheus.condini@gmail.com>
2018-09-14 09:11:53 -0300

    Use .env on firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const config = {
-  apiKey: 'AIzaSyAv0KoduS70em1Rvo4tjT6WBMLC0gq-4tg',
-  authDomain: 'in-ponto-2018.firebaseapp.com',
-  databaseURL: 'https://in-ponto-2018.firebaseio.com',
-  projectId: 'in-ponto-2018',
-  storageBucket: 'in-ponto-2018.appspot.com',
-  messagingSenderId: '829565391085',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 }
 
 export const firebaseInit = firebase.initializeApp(config)
